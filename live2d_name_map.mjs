63b2b97e7a4f89ce76c1b08650896cf4890d261d
Ruxvania <132949475+Ruxvania@users.noreply.github.com>
2026-04-15 22:11:13 -0400

    Fix finding data for paintings with caps

--- live2d_name_map.mjs
+++ live2d_name_map.mjs
@@ -5,31 +5,23 @@ import skin_data_statistics from './data/ship_data_statistics.json' with { type:
 const live2d_paintings = get_directories("./live2d");
 const painting_name_map = [];
 
-console.log(skin_data_statistics[705023])
-
 for (const painting of live2d_paintings) {
     let name;
     let skin_id;
     let ship_group;
     let ship_name;
     for (const key of Object.keys(skin_data)) {
-        if (skin_data[key].painting.toLowerCase() === painting.replace('_hx', '')) {
+        if (skin_data[key].painting.toLowerCase() === painting.replace('_hx', '').toLowerCase()) {
             name = skin_data[key].name;
             skin_id = key;
             ship_group = skin_data[key].ship_group;
         }
     }
 
-    console.log({painting, name, skin_id, ship_group})
-
     for (const key of Object.keys(skin_data_statistics)) {
         if (key.startsWith(ship_group)) {
             ship_name = skin_data_statistics[key].name;
         }
-    }
-
-    if (!name) {
-        console.warn(`Name not found for ${painting}`);
     }
 
     painting_name_map.push({
@@ -38,9 +30,12 @@ for (const painting of live2d_paintings) {
         skin_id: skin_id,
         ship_name: ship_name
     });
+
+    if (!name || !ship_name) {
+        console.warn(`Incomplete data for ${painting}`);
+    }
 }
 
-console.log(painting_name_map);
 fs.writeFileSync('./maps/live2d_name.map.json', JSON.stringify(painting_name_map), 'utf8');
 
 function get_directories(listed_directory) {
